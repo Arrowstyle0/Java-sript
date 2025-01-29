f6dd1d8209348ec4d0e0ee1782e458350c0e43d1
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-01-29 16:54:50 +0000

    nom nom

--- javaprac/java.js
+++ javaprac/java.js
@@ -18,14 +18,31 @@ const ino = "56"
 
 // console.log(name + ino);
 
-console.log(`hello my name is ${name} any my repo count is ${ino}` );
+// console.log(`hello my name is ${name} any my repo count is ${ino}` );
 
-const gameName = new String('ga')
+const gameName = new String('responsible for printing the range of ')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.toUpperCase);
+// console.log(gameName.toUpperCase);
 
+console.log(gameName.charAt(1));
 
+const newfile = gameName.substring(1,4);
 
+// console.log(newfile);
+
+// the substring() {funct} is responsible for printing the range of characters from starting index postition value to ending value 
+// or depends on user (it only prints the first value )
+
+const newstr = "  arrow   "
+console.log(newstr);
+console.log(newstr.trim());
+
+const url = "https://google.com/ans454$h"
+
+console.log(url.replace('45','hs'));
+console.log(url.includes('da'));
+
+console.log(gameName.split([' ']));
