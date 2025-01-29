addcfbe2533f6f7a9a6d3565a01d28234861b3b2
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-01-29 16:30:29 +0000

    hello

--- javaprac/java.js
+++ javaprac/java.js
@@ -1,3 +1,31 @@
 //  memory in javaj  scrip
 
 // stack (primitive dt) , stack (Non Primitive dt)
+
+let username = "arrowstyle"
+
+let tuif = username
+
+tuif = "rick"
+
+// console.log(tuif);
+
+let user = "email"
+
+const name  = "data"
+
+const ino = "56"
+
+// console.log(name + ino);
+
+console.log(`hello my name is ${name} any my repo count is ${ino}` );
+
+const gameName = new String('ga')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase);
+
+
+
