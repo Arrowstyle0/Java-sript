9643b77a4942205e05c084b5ada5976fd3106b5d
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-01-30 18:09:44 +0000

    arrayfile

---
+++ javaprac/javaarray.js
@@ -0,0 +1,25 @@
+//array
+
+const thearray  = [1,2,3,4,5,7];
+
+const thestrarray = ["batman","joker","venom"];
+
+// array methods
+
+// console.log(thearray[3]);
+
+// console.log(thestrarray.push("batwomen"));
+
+// console.log(thestrarray);
+
+// console.log(thestrarray.pop([2]));
+
+// console.log(thestrarray);
+
+console.log(thearray.unshift("domi"));
+// console.log(thearray);
+
+const myar = thearray.slice(1,4);
+
+console.log(myar);
+
