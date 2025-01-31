1cd4075be94fc09f07348340da2de80b3ea632b4
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-01-31 16:27:19 +0000

    objects

---
+++ javaprac/objectsjava.js
@@ -0,0 +1,31 @@
+const app = {}
+
+app.id =  "dajknda" 
+
+app.data = "index.1"
+
+app.lcking = "456"
+
+// console.log(app);
+
+const reguser = {
+
+    email : "someone@gmail.com",
+    fullName : {
+        firstName : "DELTA",
+        lastname : "MODE"
+    }
+}
+
+// console.log(reguser.fullName);
+
+const  pnj = {1 : "a", 2 : "b" };
+const pnj2 = {3 : "a", 4 : "b"} ;
+
+const pnj3 = {pnj, pnj2};
+
+const obj3 = Object.assign(pnj,pnj2)
+
+// console.log(pnj3);
+
+console.log(obj3);
