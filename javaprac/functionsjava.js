abef093affc6a4adc82b4711075213308833d47a
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-02-03 16:03:02 +0000

    fc in java

---
+++ javaprac/functionsjava.js
@@ -0,0 +1,42 @@
+// funtion java
+
+function calctheno(val1 ,val2 ,...theFirstnum) {
+
+    return theFirstnum
+    
+}
+
+    console.log(calctheno(456,34,456));
+    
+
+    const supplier = {
+
+        suppName : "Rick",
+        suppAge : 45,
+        suppbuiss : "Games"
+    }
+
+function handleobjc(anyuser) {
+            
+    // console.log(`The given data shows following ${anyuser.suppName}`);
+}
+
+    console.log(handleobjc())
+
+    const myNewarr = [ 200,455,567,786]
+    
+
+    function returnsecondvalue(getArray) {
+        return getArray[1]
+    
+    const  data = {
+        userName : "Ayush",
+        userAge : 23,
+        
+    }
+
+    }
+
+    console.log(returnsecondvalue([ 200,455,567,786]));
+
+    
