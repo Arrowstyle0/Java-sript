69565b595b8e67b2dcc6b9d83e1db33ccb993940
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-02-05 18:20:36 +0000

    scopes in java

---
+++ javaprac/_scopes.js
@@ -0,0 +1,61 @@
+let value  = 450
+
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30;}
+// } 
+//  console.log();
+ 
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+ 
+
+function one() {
+    const userName = "ANSH"
+
+    function two() {
+        const website = "youtube"
+        // console.log(userName);
+        
+    }
+    // console.log(website);
+    
+    two()
+}
+
+one()
+
+
+
+
+if (true) {
+    const userName = "ansh"
+    if (userName === "ansh") {
+    const website = "youtube"
+    // console.log(userName + website);
+
+    }
+    // console.log(website);
+    
+}
+
+// console.log(userName);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++interesting
+
+function addone(num)
+    {
+        return +1
+
+    }
+    addone(5)
+
+const addtwo = function(num){
+    return + 2
+}
+
+addtwo(5)
