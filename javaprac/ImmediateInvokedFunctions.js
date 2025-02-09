f814b2192876ce01a818bf6c15f95d273dc832db
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-02-09 17:42:49 +0000

    Iteration

---
+++ javaprac/ImmediateInvokedFunctions.js
@@ -0,0 +1,13 @@
+
+//IFE - TO PREVENT THE POLLUTION IN GLOBAL SCOPE
+
+(function dwarf() {
+    console.log(`db CONNECTED`);    
+    
+}) ()
+
+(function ar(){
+    console.log(`DB conn`);
+
+    
+})()
---
+++ javaprac/loops.js
@@ -0,0 +1,15 @@
+// looping statements (iterations)
+
+    a  = 34
+    b  = 100
+
+
+    if (a > b) {
+        console.log("THE FIRST NO IS GREATER THAN SECOND NO");
+        
+    }
+
+    else 
+        console.log("THE SECOND NO IS GREATER THAN FIRST NO");
+        
+
