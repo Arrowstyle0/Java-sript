559ad9170ab2b866477bed54d40f53f37f7d0ea3
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-02-11 17:23:28 +0000

    highorderarrayloops

---
+++ javaprac/highOrderarrloops.js
@@ -0,0 +1,64 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+
+const arr =[1,2,3,4,5,6,7]
+
+for (const val of arr) {
+    
+    // console.log(val);
+
+}
+
+const greetings  = "Hello ALPHA!"
+    for (const greet of greetings) {
+        // console.log(`Each character is ${greet}`);
+           
+    }
+
+// MAPS
+
+const map = new Map()
+map.set('BRI', 45);
+map.set('da', 46);
+map.set('dawg',45);
+
+// console.log(map)
+
+for (const [val,value] of map) {
+    
+    // console.log(val, ':-', value);
+    
+}
+
+const myObject  = {
+    'game1'  : 'MAXPYNE',
+    'game'   : 'GrandTheifAuto'
+
+}
+
+// for (const uio of myObject) {
+//     console.log(uio);
+     
+// }
+
+
+const myfield  = {
+    operator : 'figma',
+    lang : 'JS(JAVA SCRIPT)',
+    databases : 'mongoDB',
+    swift : "wbjda"
+}
+
+for (const key in myfield) {
+        console.log( key );
+        
+    }
+for (const key in myObject) {
+
+    console.log();
+    
+
+}
