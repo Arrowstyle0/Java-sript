da985405bd65ed1c4595e909123b7ed9c0e5eedc
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-02-14 17:00:57 +0000

    map reduce

---
+++ javaprac/mapReduce.js
@@ -0,0 +1,15 @@
+
+
+const items = ["py", "java","js","iosteam"]
+
+//   const value =   items.forEach( (prod) => {
+        // console.log(prod);
+
+    } )
+
+// console.log(value);
+
+const numbers = [10,200,30,40,50,60]
+
+numbers.filter( (num) => num > 4)
+
