c3e40b2cd66c0532ae334c6c1be1605aa6db52df
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-03-10 15:37:41 +0000

    jav practice

---
+++ javaprac/javaprac.js
@@ -0,0 +1,11 @@
+function dataRead() {
+
+const products_data1 =  ["vegies","fruits","home kitchen","food essentials"]
+const products_data2 =  ["tools","lights","safety products","toys"]
+
+dataRead()
+console.log( dataRead());
+
+ };
+
+console.log(dataRead());
