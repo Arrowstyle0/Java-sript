3c597c50bcb200609149a12eef32b39744d991c3
Arrowstyle0 <124452465+Arrowstyle0@users.noreply.github.com>
2025-03-10 15:53:40 +0000

    array to uppercase

--- javaprac/javaprac.js
+++ javaprac/javaprac.js
@@ -1,11 +1,6 @@
-function dataRead() {
+const products_data1 =  ['vegies','fruits','home kitchen','food essentials']
+const products_data2 =  ['tools','lights','safety products','toys']
 
-const products_data1 =  ["vegies","fruits","home kitchen","food essentials"]
-const products_data2 =  ["tools","lights","safety products","toys"]
-
-dataRead()
-console.log( dataRead());
-
- };
-
-console.log(dataRead());
+console.log(products_data1.at(0).toUpperCase(), products_data1.at(1).toUpperCase(), products_data1.at(2).toUpperCase(), products_data1.at(3).toUpperCase());
+console.log(products_data2.at(0).toUpperCase(), products_data2.at(1).toUpperCase(), products_data2.at(2).toUpperCase(), products_data2.at(3).toUpperCase());
+console.log(products_data1.join(products_data2).toUpperCase().);
